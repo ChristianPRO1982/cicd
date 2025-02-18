d4aa60d295cb125684727610c015387821a4f41a
Christian Martiny <christian.martiny@outlook.com>
2025-02-18 09:34:41 +0100

    feat: sub

--- test/test_math1.py
+++ test/test_math1.py
@@ -1,5 +1,5 @@
 import pytest
-from math1 import add
+from math1 import add, sub
 
 
 def test_add():
@@ -7,3 +7,9 @@ def test_add():
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
     assert add(10, 100) == 110
+
+def test_sub():
+    assert sub(2, 3) == -1
+    assert sub(-1, 1) == -2
+    assert sub(0, 0) == 0
+    assert sub(10, 100) == -90
